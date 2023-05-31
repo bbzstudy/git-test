e5cdac618fd84a341700e75a1f3d68873b1a5768
bbz <2945201386@qq.com>
2023-05-31 09:26:35 +0800

    test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-
+        System.out.println("666666666666666");
     }
 }
--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -3,10 +3,10 @@ package com.atguigu.git;
 /**
  * @Author: Bbz
  * @verson: 1.0
- * @Date: 2023/5/31 9:20
+ * @Date: 2023/5/31 9:25
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello world");
     }
 }
