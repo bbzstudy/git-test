2ec7bfa80ab4954c38588cb87964f0d029d7d5c6
bbz <2945201386@qq.com>
2023-05-31 10:03:04 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git2");
         System.out.println("666666666666666");
     }
 }
