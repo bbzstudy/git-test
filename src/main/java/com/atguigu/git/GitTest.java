5ae650ca68d41b75ecfd5a0f1c0d3488577d7d82
bbz <2945201386@qq.com>
2023-05-31 10:05:00 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git2");
         System.out.println("666666666666666");
+        System.out.println("hello git3");
+
     }
 }
