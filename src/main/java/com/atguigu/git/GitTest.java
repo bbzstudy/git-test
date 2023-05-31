26d7653775c5053082200862de217d3581f522ac
bbz <2945201386@qq.com>
2023-05-31 09:24:46 +0800

    test

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Bbz
+ * @verson: 1.0
+ * @Date: 2023/5/31 9:20
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/atguigu/git/Hello.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Bbz
+ * @verson: 1.0
+ * @Date: 2023/5/31 9:20
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
